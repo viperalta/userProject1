60a773502610479a62fffe5c1af9096adacc68ab
Vicente <viperalta@gmail.com>
2021-05-14 13:11:05 -0400

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,43 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-import './App.css';
-import LoginForm from './components/LoginForm';
-import Login from './views/Login';
-import Register from './views/Register';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import Login from "./views/Login";
+import Register from "./views/Register";
 
 function App() {
   return (
     <div className="App">
-
+      
 
       <Router>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Registro</Link>
+          </li>
+        </ul>
+      </nav>
+
+
         <Switch>
-          <Route path='/login'>
+          <Route path="/login">
             <Login />
           </Route>
-          <Route path='/register'>
-          <Register />
+          <Route path="/register">
+            <Register />
           </Route>
-          <Route path='/'>
-
+          <Route path="/">
+            <h3>Probando login y registro</h3>
           </Route>
         </Switch>
-
       </Router>
-
-      
-      
-      
     </div>
   );
 }
